2a036145f3c0075b3b93a82d1a56183b1e047c5d
bowenlin1101 <bowenblinblue@gmail.com>
2023-11-25 18:00:26 -0500

    added react components

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Notes from "./notes/notes.js"
+import Logo from "./logo/logo.js"
+import Timer from "./timer/timer.js"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <div>
+  <div id="app-container">
+    <Logo></Logo>
+    <Timer></Timer>
     <Notes></Notes>
   </div>
 );
