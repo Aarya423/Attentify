9125002b98003ce3489b28ee10b42b5b268d429d
apate695 <apate695@uwo.ca>
2023-11-25 16:17:16 -0500

    Implemented basic OpenAI chatbot.

--- chatbot.js
+++ chatbot.js
@@ -1,22 +1,20 @@
-import {Configuration, OpenAIApi} from 'openai';
+import OpenAI from 'openai';
 import dotenv from 'dotenv';
-const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-});
+dotenv.config();
 
-const openai = new OpenAIApi(configuration);
+const openai = new OpenAI(process.env.OPENAI_API_KEY);
 
-// const express = require('express');
-// const app = express();
-// app.use(express.json());
-
-async function main() {
-    const gptResponse = await openai.createChatCompletion({
-        model: 'gpt-3.5-turbo',
-        messages: [{
-            role: 'user', content: 'Hello, how are you?'
-        }]
-    });
-    console.log(gptResponse.data.choices[0]);
+async function getChatCompletion() {
+    try {
+        const chatCompletion = await openai.chat.completions.create({
+            messages: [{ role: "user", content: "Say this is a test" }],
+            model: "gpt-3.5-turbo",
+        });
+        console.log(chatCompletion.choices[0].message.content);
+    } catch (error) {
+        console.error(error);
+    }
 }
-main();
+
+getChatCompletion();
+
