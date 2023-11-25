d7bace650a4e5512d2244447656db873fce9b1cd
bowenlin1101 <bowenblinblue@gmail.com>
2023-11-25 17:59:28 -0500

    added logo button

---
+++ src/logo/logo.js
@@ -0,0 +1,11 @@
+import button from "./Logo.png"
+import "./logo.css"
+
+function Logo() {
+    return (
+        <a href="" target="_blank">
+            <img className="logo" src={button}></img>
+        </a>
+    )
+}
+export default Logo
