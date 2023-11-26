6e1d1c86fa9b9b4d141f7af18c1e340d6ad6f28f
bowenlin1101 <bowenblinblue@gmail.com>
2023-11-25 20:03:13 -0500

    integrated webpage timer with popup timer

--- public/mainPage/main.js
+++ public/mainPage/main.js
@@ -11,3 +11,25 @@ document.getElementById('chat-form').addEventListener('submit', async (event) =>
     const data = await response.json();
     document.getElementById('bot-response').textContent = data.message;
 });
+
+//Timer
+
+chrome.runtime.onMessage.addListener((request,sender,sendResponse) => {
+    if (request.query === "time") {
+        document.querySelector("#time").innerText = `${request.minutes}:${request.seconds}`;
+    } else {
+        console.log("broken")
+    }
+})
+
+document.addEventListener("click", (event) => {
+    if (event.target.id == "work") {
+        chrome.runtime.sendMessage({query: "start", time: `${25}`}, function(response) {
+            console.log("Response from background:", response);
+        });
+    } else if (event.target.id == "break") {
+        chrome.runtime.sendMessage({query: "start", time: `${10}`}, function(response) {
+            console.log("Response from background:", response);
+        });
+    }
+})
