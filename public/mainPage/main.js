387e224143f9e6df0bdca3dda7a8453812379efa
fazeeh <faseehnaqvi100@gmail.com>
2023-11-25 20:23:03 -0500

    Attempting sidebar + chatbox

--- public/mainPage/main.js
+++ public/mainPage/main.js
@@ -11,3 +11,20 @@ document.getElementById('chat-form').addEventListener('submit', async (event) =>
     const data = await response.json();
     document.getElementById('bot-response').textContent = data.message;
 });
+
+let buttonToggle = true;
+
+function toggleSidebar() {
+    const sidebar = document.getElementById("chat");
+    const button = document.getElementById("toggle-Chat");
+
+    buttonToggle = !buttonToggle;
+
+    if (buttonToggle) {
+        sidebar.style.display = "none";
+        console.log("collapse");
+    } else {
+        sidebar.style.display = "flex";
+        console.log("show");
+    }
+}
